c26c81d5efcdd64f466395141099cfe0da3f5fed
Andrew Marshall <48880490+gingerninja0@users.noreply.github.com>
2026-01-14 21:53:43 +1100

    I first attempted the advent of code in MATLAB
    These are my solutions to day 1 part 1 and part 2 in MATLAB. Its written such that the conditions in the while loops match the operations of my fpga (verilog/hardcaml) processor, so they look a bit strange

---
+++ src/MATLAB solutions/day_1_part_2.m
@@ -0,0 +1,47 @@
+clear
+clc
+
+data = importdata("input.txt");
+
+len = length(data);
+
+dial_val = 50; % Initially the safe dial starts at 50
+zero_count = 0; 
+
+for i = 1:len
+
+    temp = data(i);
+    temp = temp{1};
+
+    dir = temp(1);
+    num = str2double(temp(2:end));
+
+    if strcmp(dir,"L")
+        dial_val = dial_val - num;
+    else
+        dial_val = dial_val + num;
+    end
+
+    % This condition looks weird because it matches the operation in the
+    % fpga processor
+    % Normally it would be: dial_val < 0
+    while dial_val + 0 < 0
+        dial_val = 100 + dial_val;
+        % If we're in one of these cases, we're crossing over 0 (for part 2)
+        zero_count = zero_count + 1; % Uncomment for part 2, comment out for part 1
+    end
+
+    % This condition looks weird because it matches the operation in the
+    % fpga processor
+    % Normally it would be: dial_val > 99
+    while 99 - dial_val < 0  
+        dial_val = dial_val - 100;
+        % If we're in one of these cases, we're crossing over 0 (for part 2)
+        zero_count = zero_count + 1; % Uncomment for part 2, comment out for part 1
+    end
+
+end
+
+zero_count
+
+% zero_count = 6819 (as required)
